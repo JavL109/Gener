c2d100e56c0016b364739866de24472e24b7dd5d
JavL109 <jlopez21167@iesjoanramis.org>
2026-02-02 13:57:05 +0100

    Add files via upload

---
+++ Gener/Día 4/arrays.js
@@ -0,0 +1,14 @@
+let result = 0;
+
+const comidas = ["plátano", "manzana", "pizza", "helado", "mango"];
+for (let i=0;i < comidas.length;i++){
+    console.log(comidas[i])
+}
+
+const nums = [1,2,3,4,5]
+
+for (let i=0; i<nums.length; i++){
+    result += nums[i]; 
+}
+console.log(result)
+alert (result)
